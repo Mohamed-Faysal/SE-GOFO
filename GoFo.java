0a7eb8a171c0bdb0066c94b8029b8d7728801c2c
Mohamed <20190711@stud.fci-cu.edu.eg>
2021-06-08 02:56:13 +0200

    Adding 2 src files

---
+++ GoFo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GoFo {
+
+	public static void main(String[] args) {
+		Playgrounds [] playgrounds;
+		Player [] players;
+		PlaygroundOwner [] playgroundOwners;
+		int choice;
+		Scanner input = new Scanner(System.in);
+		while(true) {
+			System.out.println("Welcom To Gofo App \n 1-Register as Player \n 2-Register as playground owner");
+			choice=input.nextInt();
+			break;
+		}
+	}
+
+}
