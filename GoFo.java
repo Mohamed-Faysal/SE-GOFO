95d26694bf5984c2c220e7f4ebc76ce2c5a21735
Mohamed <20190711@stud.fci-cu.edu.eg>
2021-06-08 05:38:44 +0200

    Updated the main and playgrounds class

--- GoFo.java
+++ GoFo.java
@@ -1,17 +1,100 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
+/*notes
+ * you can make the id = counter for the user so he can access his account any time 
+ * make playgrounds counter
+*/
 public class GoFo {
 
 	public static void main(String[] args) {
-		Playgrounds [] playgrounds;
-		Player [] players;
-		PlaygroundOwner [] playgroundOwners;
-		int choice;
+		ArrayList<Playgrounds> playgrounds = new ArrayList<Playgrounds>();
+		ArrayList<Player> players = new ArrayList<Player>();
+		ArrayList<PlaygroundOwner> playgroundOwners = new ArrayList<PlaygroundOwner>();
+		Player player = new Player();
+		PlaygroundOwner playgroundOwner = new PlaygroundOwner();
+		Playgrounds playground = new Playgrounds();
+		int choice,counter=-1;
+		String name;
+		boolean bool=true;
 		Scanner input = new Scanner(System.in);
-		while(true) {
-			System.out.println("Welcom To Gofo App \n 1-Register as Player \n 2-Register as playground owner");
+		//for playgrounds
+		int pgCounter=-1;
+		double price;
+		while(bool) {
+			counter++;
+			System.out.println("Welcom To Gofo App \n 1-Register as Player \n 2-Register as playground owner\n 3-Quit");
 			choice=input.nextInt();
-			break;
+			if(choice==1) {
+				
+				System.out.println("Enter your Name");
+				name=input.next();
+				player.setName(name);
+				players.add(player);
+				System.out.println("Welcome "+players.get(counter).getName());
+				System.out.println("1-Book a Playground \n2-Return\n3-Quit");
+				choice=input.nextInt();
+				switch(choice) {
+				case 1:
+					break;
+				case 2:
+					counter--;
+					break;
+				case 3:
+					bool=false;
+					break;
+				default:
+					System.out.println("Invalid choice, Quiting...");
+					bool=false;
+					break;
+				}
+				
+			} else if (choice==2) {
+				System.out.println("Enter your Name");
+				name=input.next();
+				playgroundOwner.setName(name);
+				playgroundOwners.add(playgroundOwner);
+				System.out.println("Welcome "+playgroundOwners.get(counter).getName());
+				System.out.println("1-Add a Playground \n2-Return\n3-Quit");
+				choice=input.nextInt();
+				switch(choice) {
+				case 1:
+					pgCounter++;
+					System.out.println("Enter the price");
+					price=input.nextDouble();
+					System.out.println("The available slots are \n1-(10 to 12 AM)  \n2-(12 to 2 PM)"
+							+ ", 3-(2 to 4 PM), 4-(4 to 6 PM), 5-(6 to 8 PM)"
+							+ "\n6-(8 to 10 PM), 7-(10 to 12 PM)"
+							+ "\nHow many slots are available? and which?");
+					choice=input.nextInt();
+					int[] slots = new int [choice];
+					for(int i=0;i<choice;i++) {
+						slots[i]=input.nextInt();
+					}
+					playground.setPrice(price);
+					playground.setTime(slots, choice);
+					playgrounds.add(playground);
+					System.out.println("Added "+playgrounds.get(pgCounter).toString());
+					break;
+				case 2:
+					counter--; //Return the counter to the same indexing number in the array list (always zero because there is no sign in and you just register one time)
+					break;
+				case 3:
+					bool=false;
+					break;
+				default:
+					System.out.println("Invalid choice, Quiting...");
+					bool=false;
+					break;
+				}
+			}
+			else if (choice==3) {
+				bool=false;
+			}
+			else {
+				System.out.println("Invalid choise, Quiting...");
+				break;
+			}
+			
 		}
 	}
 
--- Playgrounds.java
+++ Playgrounds.java
@@ -1,7 +1,8 @@
 
 public class Playgrounds {
 	private double price;
-	private int [] times;
+	private int [] times= new int [3];
+	private int Size;
 	public static int id=0;
 	public Playgrounds() {
 		id++;
@@ -9,14 +10,46 @@ public class Playgrounds {
 	public void setPrice(double price) {
 		this.price=price;
 	}
-public void setTime(int [] times) {
-		this.times=times;
+public void setTime(int [] times,int size) {
+	this.Size=size;
+		for(int i=0;i<size;i++) {
+			this.times[i]=times[i];
+		}
 	}
 public double getPrice() {
 	return price;
 }
-public int[] getTimes() {
-	return times;
+public String getTimes() {
+	String timeSlot="";
+	int slot=0;
+	for(int i=0;i<Size;i++) {
+		slot=times[i];
+	switch (slot){
+	case 1:
+		timeSlot+=" 10 to 12 AM";
+	break;
+	case 2:
+		timeSlot+=" 12 to 2 PM";
+	break;
+	case 3:
+		timeSlot+=" 2 to 4 PM";
+	break;
+	case 4:
+		timeSlot+=" 4 to 6 PM";
+	break;
+	case 5:
+		timeSlot+=" 6 to 8 PM";
+	break;
+	case 6:
+		timeSlot+=" 8 to 10 PM";
+	break;
+	case 7:
+		timeSlot+=" 10 to 12 PM";
+	break;
+	}//switch
+	}//for loop
+	
+	return timeSlot;
 }
 	public String toString() {
 		return "Price is :"+getPrice()+" times: "+getTimes() ;
